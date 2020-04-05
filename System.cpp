6e8efceabf473cadb6ae457c012e384abc5ca559
Ota-cueille <vianneyherve3@gmail.com>
2020-04-05 13:01:23 +0200

    before pulling

--- System.cpp
+++ System.cpp
@@ -2,8 +2,7 @@
 
 void System::addComponent(Component& comp)
 {
-	if (!(this->listComponents.emplace(comp.getID(), 
-		std::vector<std::reference_wrapper<Component>>{ comp })).second)
+	if (!(this->listComponents.emplace(comp.getID(), std::vector<std::reference_wrapper<Component>>{ comp })).second)
 	//this means compo.get_id() is aleady a key
 	{
 		this->listComponents[comp.getID()].push_back(comp);
--- System.hpp
+++ System.hpp
@@ -7,8 +7,7 @@
 class System
 {
 private:
-	std::map<ID, 
-		std::vector<std::reference_wrapper<Component>>> listComponents;
+	std::map<ID, std::vector<std::reference_wrapper<Component>>> listComponents;
 public:
 	virtual void update() = 0; //the wole fonc
 
