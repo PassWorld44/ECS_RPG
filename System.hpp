aaa1abca39750dbc8f44da708992189759219a46
Emile BONDU <emile.bondu@gmail.com>
2020-04-05 12:57:55 +0200

    rename a var

--- System.hpp
+++ System.hpp
@@ -7,8 +7,8 @@
 class System
 {
 private:
-	std::map<ID, 
-		std::vector<std::reference_wrapper<Component>>> listComponents;
+	std::map<ID, std::vector<std::reference_wrapper<Component>>> 
+														listComponent;
 public:
 	virtual void update() = 0; //the wole fonc
 
