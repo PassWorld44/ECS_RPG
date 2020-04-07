b74751d2ca74cf4c815de649bb3889d323c4663a
Ota-cueille <vianneyherve3@gmail.com>
2020-04-07 11:34:59 +0200

    juste pour save

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 	std::vector<ID> RectTestListID;
 	RectTestListID.push_back(e.addEntity());
 	e.addComponent<componentPosition>(RectTestListID[0],0,0);
-	//e.addComponent<componentDimension>(RectTestListID[0],{RectTestListID[0],100,100});
+	e.addComponent<componentDimension>(RectTestListID[0],100,100);
 
 	return 0;
 }
