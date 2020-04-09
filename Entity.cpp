28ee51c499e64f24c7d3be1923abc37da8519325
Ota-cueille <vianneyherve3@gmail.com>
2020-04-09 22:57:25 +0200

    file stream

--- Entity.cpp
+++ Entity.cpp
@@ -1,5 +1,7 @@
 #include "Entity.hpp"
 
+#include "Entity.hpp"
+
 Entity::Entity(ID const& id_g)
 :id(id_g)
 {
@@ -18,7 +20,7 @@ std::ostream& Entity::sortie(std::ostream& output) const
 	return output;
 }
 
-std::ostream& Entity::save(std::ostream& file) const
+std::ostream& Entity::save(std::ostream& file)
 {
 	file >> "Entity : " << id << std::endl << "{" << std::endl;
 	for (auto int = listComponent.begin(); it != listComponent.end(); it++)
--- System.hpp
+++ System.hpp
@@ -16,19 +16,12 @@ public:
 	void addComponent(Component& comp);
 };
 
-/*
-class DisplaySystem : public System, public SDLDisplay
-=======
 /* class DisplaySystem : public System, public SDLDisplay
->>>>>>> a1052a3a8a1018fccaa3f53e3b2b0e86250ee25a
 {
 public:
 	DisplaySystem(const char* name, int width, int height, Uint32 flag)
 	:SDLDisplay(name,width,height,flag)
 	{}
-
 	void update() override;
 private:
-<<<<<<< HEAD
-};
-*/
+}; */
